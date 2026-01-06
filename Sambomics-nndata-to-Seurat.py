c8b92275114c63db2eed616209e9bf16f51d89ea
adajg <A.jimenez-gonzalez@sheffield.ac.uk>
2026-01-06 16:12:02 +0000

    Script to convert h5ad files to seurat-readable

---
+++ Sambomics-nndata-to-Seurat.py
@@ -0,0 +1,24 @@
+## This script converts h5ad anndata to seurat-readable files.
+## The script has been copied from sanbomics_scripts (mousepixels - github).
+
+import scanpy as sc
+from scipy import io
+!mkdir matrix_files
+!mkdir 
+adata = sc.read_h5ad("zf_atlas_24hpf_v1_release.h5ad")" 
+adata
+
+with open('matrix_files/barcodes.tsv', 'w') as f:
+    for item in adata.obs_names:
+        f.write(item + '\n')
+        
+
+with open('matrix_files/features.tsv', 'w') as f:
+    for item in ['\t'.join([x,x,'Gene Expression']) for x in adata.var_names]:
+        f.write(item + '\n')
+        
+io.mmwrite('matrix_files/matrix', adata.X.T)
+
+
+!ls matrix_files/
+!gzip matrix_files/*
