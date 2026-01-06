f21fa764a32dabcfc23edfb95f2f7fbea930ec0d
Ada Jimenez-Gonzalez <A.jimenez-gonzalez@sheffield.ac.uk>
2026-01-06 16:13:05 +0000

    Fixed a typo

--- Sambomics-nndata-to-Seurat.py
+++ Sambomics-nndata-to-Seurat.py
@@ -5,7 +5,7 @@ import scanpy as sc
 from scipy import io
 !mkdir matrix_files
 !mkdir 
-adata = sc.read_h5ad("zf_atlas_24hpf_v1_release.h5ad")" 
+adata = sc.read_h5ad("zf_atlas_24hpf_v1_release.h5ad") 
 adata
 
 with open('matrix_files/barcodes.tsv', 'w') as f:
